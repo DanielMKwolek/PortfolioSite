7a187e8177d04da5e8724ab13f7f8e4540c0f5ed
Danny Kwolek <Daniel.m.kwolek@gmail.com>
2022-04-27 13:54:02 -0700

    added social icons and profile picture

--- main.js
+++ main.js
@@ -5,13 +5,15 @@ const colors = {
     DarkEmphShadowColor: "#500",
     DarkHeaderColor: "#333",
     DarkBodyColor: "#444",
-    DarkLineColor: "#888",
+    DarkLineColor: "#000",
+    DarkTextColor: "#000",
 
     LightEmphColor: "#0f0",
     LightEmphShadowColor: "#050",
     LightHeaderColor: "#aaa",
     LightBodyColor: "#fff",
-    LightLineColor: "#888",
+    LightLineColor: "#ddd",
+    LightTextColor: "#888"
 }
 
 let internalCSS = $("<style>", {
@@ -29,7 +31,9 @@ function toggleLightMode() {
 --active-header-color: ${colors.DarkHeaderColor};
 --active-body-color: ${colors.DarkBodyColor};
 --active-line-color: ${colors.DarkLineColor};
+--active-text-color: ${colors.DarkTextColor};
 }`)
+        $("#github").attr("src", "assets/socials/GitHub_Logo_White.png");
     } else {
         lightMode = true;
         $("#internalCSS").text(`:root {
@@ -38,7 +42,9 @@ function toggleLightMode() {
 --active-header-color: ${colors.LightHeaderColor};
 --active-body-color: ${colors.LightBodyColor};
 --active-line-color: ${colors.LightLineColor};
+--active-text-color: ${colors.LightTextColor};
 }`)
+        $("#github").attr("src", "assets/socials/GitHub_Logo.png");
     }
 }
 toggleLightMode();
