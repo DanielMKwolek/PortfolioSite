e7f462c4236ec17a8324a43f42c0e04db8ecb329
Danny Kwolek <Daniel.m.kwolek@gmail.com>
2022-04-27 11:30:57 -0700

    lightMode Toggle working

--- main.js
+++ main.js
@@ -1,3 +1,46 @@
-$("#name").on("click", (event) => {
-    $("#name").text($("#mobileCode").html());
+let lightMode = false;
+
+const colors = {
+    DarkEmphColor: "#f00",
+    DarkEmphShadowColor: "#500",
+    DarkHeaderColor: "#333",
+    DarkBodyColor: "#444",
+    DarkLineColor: "#888",
+
+    LightEmphColor: "#0f0",
+    LightEmphShadowColor: "#050",
+    LightHeaderColor: "#aaa",
+    LightBodyColor: "#fff",
+    LightLineColor: "#888",
+}
+
+let internalCSS = $("<style>", {
+    id: "internalCSS"
 });
+
+$("head").append(internalCSS);
+
+function toggleLightMode() {
+    if (lightMode) {
+        lightMode = false;
+        $("#internalCSS").text(`:root {
+--active-emph-color: ${colors.DarkEmphColor};
+--active-emph-shadow-color: ${colors.DarkEmphShadowColor};
+--active-header-color: ${colors.DarkHeaderColor};
+--active-body-color: ${colors.DarkBodyColor};
+--active-line-color: ${colors.DarkLineColor};
+}`)
+    } else {
+        lightMode = true;
+        $("#internalCSS").text(`:root {
+--active-emph-color: ${colors.LightEmphColor};
+--active-emph-shadow-color: ${colors.LightEmphShadowColor};
+--active-header-color: ${colors.LightHeaderColor};
+--active-body-color: ${colors.LightBodyColor};
+--active-line-color: ${colors.LightLineColor};
+}`)
+    }
+}
+toggleLightMode();
+
+$("#modeToggle").on("click", toggleLightMode);
